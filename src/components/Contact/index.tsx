ac49cfc425e2649960563d5dcf5a76d725731cfd
Enaholo Akhere <enaholoa@gmail.com>
2025-05-14 17:58:54 +0100

    :tada: FEAT(Contact-Us): Contact us created

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,6 +1,6 @@
 const ContactComp = () => {
   return (
-    <div className=' px-4 m-auto max-w-md'>
+    <div className=' px-4 m-auto max-w-2xl'>
       <h2 className='heading text-center'>Contact Us</h2>
       <p className='mb-8 lg:mb-16 font-light text-center text__para'>
         Got a technical issue? Want to send feedback about a beta feature? Let
