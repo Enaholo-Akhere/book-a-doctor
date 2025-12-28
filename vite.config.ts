4b2fcaa2a7876ec18bd997b9edf29e9a0bfccd81
Enaholo Akhere <enaholoa@gmail.com>
2025-12-28 19:23:36 +0100

    CONFLICT fix(conflict-issues): resolution in progress

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src")
-    }
-  },
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/[name]-[hash][extname]",
-        chunkFileNames: "assets/[name]-[hash].js",
-        entryFileNames: "assets/[name]-[hash].js"
-      }
+      '@': path.resolve(__dirname, 'src'), // same as tsconfig paths
+
     }
   }
 });
