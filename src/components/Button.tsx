98cd1c41340069038a390ef59c38d2acdd1c31c3
Enaholo Akhere <69322558+Enaholo-Akhere@users.noreply.github.com>
2026-06-16 17:46:14 +0100

    chores: worked on bugs success page (#36)

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,6 +8,7 @@ const Button = ({
   btnWidth = 'w-fit',
   py = 'md:py-4 py-3',
   px = 'md:px-[30px] px-[20px] ',
+  bgHover = 'hover:bg-primaryColor2',
   classNameProps,
 }: {
   title: string;
@@ -20,13 +21,14 @@ const Button = ({
   py?: string;
   classNameProps?: string;
   disabled?: boolean;
+  bgHover?: string;
 }) => {
   return (
     <button
       onClick={onClick}
       disabled={disabled}
       type={btnType}
-      className={`${bgColor} ${txtColor} ${btnWidth} ${px} ${py} {${disabled ? 'hover:cursor-auto hover:bg-gray-700' : ' hover:bg-primaryColor2'} cursor-pointer ease-in-out font-[600] rounded-[52px] text-[12px] md:text-[16px] ${classNameProps} `}
+      className={`${bgColor} ${txtColor} ${bgHover} ${btnWidth} ${px} ${py} {${disabled ? 'hover:cursor-auto hover:bg-gray-700' : `${bgHover}`} cursor-pointer ease-in-out font-[600] rounded-[52px] text-[12px] md:text-[16px] ${classNameProps} `}
     >
       {title}
     </button>
