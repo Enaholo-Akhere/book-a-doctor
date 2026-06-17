0f92a3e0b6a6d2054fdaa5da5092c21c06e8a898
Enaholo Akhere <69322558+Enaholo-Akhere@users.noreply.github.com>
2026-06-17 13:02:25 +0100

    Chore/clean up (#42)
    * chores: worked on bugs success page
    
    * fix geolocation price and currency
    
    * fixed side panel
    
    * fixed side panel
    
    * FIX/SIDEPANEL-PRICE
    
    * ticket priced fixed
    
    * currency formatter
    
    * clean up

---
+++ src/utils/formartCurrency.ts
@@ -0,0 +1,9 @@
+export const formatCurrency = (amount: number, currency: string) => {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency,
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0,
+    }).format(amount);
+};
+
