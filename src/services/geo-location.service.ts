0de2cde7d6062c81482665b387442427a60b36c2
Enaholo Akhere <69322558+Enaholo-Akhere@users.noreply.github.com>
2026-06-17 12:27:44 +0100

    Fix/ticket price fixed (#40)
    * chores: worked on bugs success page
    
    * fix geolocation price and currency
    
    * fixed side panel
    
    * fixed side panel
    
    * FIX/SIDEPANEL-PRICE
    
    * ticket priced fixed

--- src/services/geo-location.service.ts
+++ src/services/geo-location.service.ts
@@ -1,12 +1,8 @@
-import axios from 'axios'
+import { api } from '@/library/api/axios';
 
 export const getGeolocationService = async () => {
-    const localUrl = import.meta.env.VITE_GEO_LOC_LOC;
-    const prodUrl = import.meta.env.VITE_GEO_LOC_PROD;
 
-    const geoURL = import.meta.env.MODE === "production" ? prodUrl : localUrl;
-
-    const { data } = await axios.get(`${geoURL}`);
+    const { data } = await api.get('/auth/geolocation');
     return data;
 
 };
