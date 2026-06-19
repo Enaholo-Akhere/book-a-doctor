a850bf560923c2863bca12cf5fd871a0d4af2bd9
Enaholo Akhere <69322558+Enaholo-Akhere@users.noreply.github.com>
2026-06-19 14:11:40 +0100

    Feat/currency format (#41)
    * chores: worked on bugs success page
    
    * fix geolocation price and currency
    
    * fixed side panel
    
    * fixed side panel
    
    * FIX/SIDEPANEL-PRICE
    
    * ticket priced fixed
    
    * currency formatter

--- src/components/Doctors/SidePanel.tsx
+++ src/components/Doctors/SidePanel.tsx
@@ -106,7 +106,8 @@ const SidePanel = ({ data }: { data: doctorsInterface }) => {
           <div className='flex items-center justify-between'>
             <p className='text__para mt-0 font-semibold'>Ticket Price</p>
             <span className='text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor font-bold '>
-              {formatCurrency(price, geolocation.currency)}
+              {formatCurrency(price, geolocation.currency)}{' '}
+              {geolocation?.currency}
             </span>
           </div>
           <div className='mt-[30px] '>
