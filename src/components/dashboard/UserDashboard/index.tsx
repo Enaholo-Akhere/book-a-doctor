0834500f8e8803f6f9ede9d074b737fcee9af881
Enaholo Akhere <enaholoa@gmail.com>
2026-06-24 22:24:16 +0100

    chores

--- src/components/dashboard/UserDashboard/index.tsx
+++ src/components/dashboard/UserDashboard/index.tsx
@@ -12,6 +12,7 @@ import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 import { handleAxiosError } from '@/utils/axiosError';
 import { useCutTitle } from '@/Hook/useTextCut';
+import ChatWindow from '@/components/ChatRooms/Chat';
 
 const UserDashboard = () => {
   const user = useAuthStore((state) => state.user);
@@ -41,6 +42,8 @@ const UserDashboard = () => {
   };
 
   const [tab, setTab] = useState<string>('bookings');
+  const [showChat, setShowChat] = useState<boolean>(false);
+
   const errMsg = error as AxiosError;
   const errorData = errMsg?.response?.data as { message?: string };
   return (
@@ -48,65 +51,83 @@ const UserDashboard = () => {
       {isLoading && <Loading />}
       {isError && <Error errorMessage={errorData?.message} />}
       {!isError && !isLoading && data && (
-        <div className='grid md:grid-cols-3 gap-10 '>
-          <div className='pb-[50px] px-[30px] rounded-md '>
-            <div className='flex items-center justify-center'>
-              <figure className='w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor '>
-                <img
-                  src={data.data.photo?.imageUrl}
-                  alt='user image'
-                  className='w-full h-full rounded-full'
+        <>
+          <p
+            className='cursor-pointer'
+            onClick={() => setShowChat((prev) => !prev)}
+          >
+            Chat Doctor
+          </p>
+          <div className='relative'>
+            <div className='absolute right-0 bottom-[100%]'>
+              <div className={`${showChat ? 'block' : 'hidden'}`}>
+                <ChatWindow
+                  currentUserId=''
+                  otherUser={{ id: '3ewdfr', name: 'John Doe' }}
                 />
-              </figure>
-            </div>
-            <div className='mt-4 text-center'>
-              <h3 className='text-[18px] leading-[30px] text-headingColor font-bold '>
-                {data.data.name}
-              </h3>
-              <p className='text-textColor text-[15px] leading-6 font-medium  '>
-                {data.data.email}
-              </p>
-              <p className='text-textColor text-[15px] leading-6 font-medium  '>
-                Blood Type:{' '}
-                <span className='ml-2 text-headingColor text-[22px] leading-8  '>
-                  {data.data.bloodType || 'Not set'}
-                </span>
-              </p>
-            </div>
-            <div className='mt-[50px] md:mt-[100px]'>
-              <button
-                onClick={handleLogout}
-                className='w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white  '
-              >
-                {isPending ? 'Logging out...' : 'Logout'}
-              </button>
-              <button className='w-full bg-red-600 mt-4 p-3 text-[16px] leading-7 rounded-md text-white '>
-                Delete account
-              </button>
+              </div>
             </div>
           </div>
-          <div className='md:col-span-2 md:px-[30px] '>
-            <div className='px-8 flex gap-2 justify-center'>
-              <Button
-                title={bookingTitle}
-                onClick={() => setTab('bookings')}
-                txtColor='gray-800'
-                bgColor={`${tab === 'bookings' && 'bg-primaryColor text-white '}`}
-                classNameProps={`font-normal p-2 mr-5 px-5 rounded-md text-headingColor font-semibold text-[16px] leading- border border-solid border-primaryColor`}
-              />
-              <Button
-                title={profileTitle}
-                txtColor='gray-800'
-                onClick={() => setTab('settings')}
-                bgColor={`${tab === 'settings' && 'bg-primaryColor text-white '}`}
-                classNameProps={`font-normal p-2 mr-5 px-5 rounded-md text-headingColor font-semibold text-[16px] leading- border border-solid border-primaryColor`}
-              />
+          <div className='grid md:grid-cols-3 gap-10 '>
+            <div className='pb-[50px] px-[30px] rounded-md '>
+              <div className='flex items-center justify-center'>
+                <figure className='w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor '>
+                  <img
+                    src={data.data.photo?.imageUrl}
+                    alt='user image'
+                    className='w-full h-full rounded-full'
+                  />
+                </figure>
+              </div>
+              <div className='mt-4 text-center'>
+                <h3 className='text-[18px] leading-[30px] text-headingColor font-bold '>
+                  {data.data.name}
+                </h3>
+                <p className='text-textColor text-[15px] leading-6 font-medium  '>
+                  {data.data.email}
+                </p>
+                <p className='text-textColor text-[15px] leading-6 font-medium  '>
+                  Blood Type:{' '}
+                  <span className='ml-2 text-headingColor text-[22px] leading-8  '>
+                    {data.data.bloodType || 'Not set'}
+                  </span>
+                </p>
+              </div>
+              <div className='mt-[50px] md:mt-[100px]'>
+                <button
+                  onClick={handleLogout}
+                  className='w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white  '
+                >
+                  {isPending ? 'Logging out...' : 'Logout'}
+                </button>
+                <button className='w-full bg-red-600 mt-4 p-3 text-[16px] leading-7 rounded-md text-white '>
+                  Delete account
+                </button>
+              </div>
             </div>
+            <div className='md:col-span-2 md:px-[30px] '>
+              <div className='px-8 flex gap-2 justify-center'>
+                <Button
+                  title={bookingTitle}
+                  onClick={() => setTab('bookings')}
+                  txtColor='gray-800'
+                  bgColor={`${tab === 'bookings' && 'bg-primaryColor text-white '}`}
+                  classNameProps={`font-normal p-2 mr-5 px-5 rounded-md text-headingColor font-semibold text-[16px] leading- border border-solid border-primaryColor`}
+                />
+                <Button
+                  title={profileTitle}
+                  txtColor='gray-800'
+                  onClick={() => setTab('settings')}
+                  bgColor={`${tab === 'settings' && 'bg-primaryColor text-white '}`}
+                  classNameProps={`font-normal p-2 mr-5 px-5 rounded-md text-headingColor font-semibold text-[16px] leading- border border-solid border-primaryColor`}
+                />
+              </div>
 
-            {tab === 'bookings' && <MyBookings user={data.data} />}
-            {tab === 'settings' && <ProfileSettings user={data.data} />}
+              {tab === 'bookings' && <MyBookings user={data.data} />}
+              {tab === 'settings' && <ProfileSettings user={data.data} />}
+            </div>
           </div>
-        </div>
+        </>
       )}
     </div>
   );
